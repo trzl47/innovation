7d0055358673fc14c24174ba5c39de675730d4ad
trzl47 <brownty47@gmail.com>
2017-04-30 23:31:52 -0500

    Footer component

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+// Libs
+import React from 'react';
+//CSS
+import css from './innovation.css';
+
+const Footer = () => (
+	<div className={[css.footer].join(' ')}>
+		<div className={[css.container,"container"].join(' ')}>
+			<p> Innovation Cloud Conference</p>
+		</div>
+	</div>
+);
+
+export default Footer;
